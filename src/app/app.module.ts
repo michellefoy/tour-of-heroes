62ba6cb9833e6becaa9bafb8153f55326a3c4c15
Michelle Foy <michelle.s.foy@gmail.com>
2018-07-23 13:45:07 -0400

    Add further components, services, and mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {HeroesComponent} from './components/heroes/heroes.component';
+import {HeroDetailComponent} from './components/hero-detail/hero-detail.component';
+import {HeroService} from './services/hero.service';
+import { MessagesComponent } from './components/messages/messages.component';
+import {MessageService} from './services/message.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroService,
+    MessageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
